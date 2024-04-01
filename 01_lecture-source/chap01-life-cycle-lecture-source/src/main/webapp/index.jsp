5e0517ac36967280a1d5c45dee0ba8ba68faf2d0
pyunghun <raccoon.ohgiraffers@gmail.com>
2024-04-01 14:08:41 +0900

    [Servlet]

--- 01_lecture-source/chap01-life-cycle-lecture-source/src/main/webapp/index.jsp
+++ 01_lecture-source/chap01-life-cycle-lecture-source/src/main/webapp/index.jsp
@@ -7,5 +7,7 @@
 <body>
     <h1>라이프 사이클 테스트</h1>
     <a href="xml-lifecycle">xml 라이프사이클 테스트</a>
+    <br>
+    <a href="annotation-lifecycle">annotation 라이프사이클 테스트</a>
 </body>
 </html>
---
+++ 01_lecture-source/chap02-service-method-lecture-source/src/main/java/com/ohgiraffers/section01/servicemethod/ServiceMethodTestServlet.java
@@ -0,0 +1,8 @@
+package com.ohgiraffers.section01.servicemethod;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet("/request-service")
+public class ServiceMethodTestServlet extends HttpServlet {
+}
