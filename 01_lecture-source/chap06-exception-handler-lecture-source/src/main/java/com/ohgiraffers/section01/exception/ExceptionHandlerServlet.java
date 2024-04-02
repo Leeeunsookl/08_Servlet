d1adc7621d3dd426c22db665b99197f92ace6854
pyunghun <raccoon.ohgiraffers@gmail.com>
2024-04-02 12:16:26 +0900

    [commit]

--- 01_lecture-source/chap06-exception-handler-lecture-source/src/main/java/com/ohgiraffers/section01/exception/ExceptionHandlerServlet.java
+++ 01_lecture-source/chap06-exception-handler-lecture-source/src/main/java/com/ohgiraffers/section01/exception/ExceptionHandlerServlet.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Enumeration;
 
 @WebServlet("/showErrorPage")
@@ -23,6 +24,34 @@ public class ExceptionHandlerServlet extends HttpServlet {
         while (attrNames.hasMoreElements()) {
             System.out.println(attrNames.nextElement());
         }
+
+        String statusCode = req.getAttribute("jakarta.servlet.error.status_code").toString();   // 404, 500 꺼냈다
+        String message = req.getAttribute("jakarta.servlet.error.message").toString();
+
+        String requestUrl = req.getAttribute("jakarta.servlet.forward.request_uri").toString();
+        System.out.println("requestUrl = " + requestUrl);
+        
+        StringBuilder errorPage = new StringBuilder();
+        errorPage.append("<!doctype html>\n")
+                .append("<html>\n")
+                .append("<head></head>\n")
+                .append("<body>\n")
+                .append("<h1>\n")
+                .append(statusCode)
+                .append(" - ")
+                .append(message)
+                .append("</h1>\n")
+                .append("</body>\n")
+                .append("</html>");
+
+        resp.setContentType("text/html; charset=UTF-8");
+
+        PrintWriter out = resp.getWriter();
+
+        out.print(errorPage.toString());
+
+        out.flush();
+        out.close();
 
     }
 }
